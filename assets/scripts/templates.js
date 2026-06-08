37cdbe38c35a5518b5d01b578677d20ab28a8364
Anne Manthey <anne.manthey@pm.me>
2026-06-08 16:27:36 +0200

    style: add background color classes

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -5,8 +5,8 @@ function getCardTemplate(cardObject, formattedName){
                 <p># ${cardObject.id}</p>
                 <h2 id="card-name">${formattedName}</h2>
             </header>
-            <main>
-                <img id="card-img" src="${cardObject.img}" alt="Pokemon">
+            <main id="card-main" class="card-main ${cardObject.types[0]}">
+                <img src="${cardObject.img}" alt="Pokemon">
             </main>
             <footer>
                 <div>
--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function renderTypes(typeList) {
 
 
 
+
 // To Do:
 
 //Gallery:
