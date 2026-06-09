460650fe29171741fb10562c4f620ac182c447f5
Anne Manthey <anne.manthey@pm.me>
2026-06-09 07:48:03 +0200

    feat: add function getDialogTemplate

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -17,3 +17,41 @@ function getCardTemplate(cardObject, formattedName){
     `
 }
 
+function getDialogTemplate(){
+    return /*html*/ `
+        <header class="dialog_header">
+            <p></p>
+            <h2></h2>
+            <button>x</button>
+        </header>
+        <main>
+            <section>
+                <img src="" alt="">
+                <div></div>
+            </section>
+            <section>
+                <div class="tab">
+                    <button class="tablinks" onclick="openTab(event, 'dialog-about')" id="defaultOpen">About</button>
+                    <button class="tablinks" onclick="openTab(event, 'dialog-stats')">Base Stats</button>
+                    <button class="tablinks" onclick="openTab(event, 'dialog-moves')">Moves</button>
+                </div>
+                <div id="dialog-about" class="tab_content">
+                    <h3>About</h3>
+                    <p>Platzhalter</p>
+                </div>
+                <div id="dialog-stats" class="tab_content">
+                    <h3>Base Stats</h3>
+                    <p>Platzhalter</p>
+                </div>
+                <div id="dialog-moves" class="tab_content">
+                    <h3>Moves</h3>
+                    <p>Platzhalter</p>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <button class="button_img"><img src="./assets/icons/arrow_left.png" alt="Arrow Left"></button>
+            <button class="button_img"><img src="./assets/icons/arrow_right.png" alt="Arrow Right"></button>
+        </footer>
+    `
+}
